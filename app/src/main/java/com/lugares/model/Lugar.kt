5aa7b2e0c0b3657f8fc4e82b30fa1ce6df5a94b0
daniialfaro9 <daniialfaro9@gmail.com>
2022-03-23 21:14:51 -0600

    Inicial
    Ejercicio Semana 8 Y 9

---
+++ app/src/main/java/com/lugares/model/Lugar.kt
@@ -0,0 +1,24 @@
+package com.lugares.model
+
+import android.os.Parcelable
+//import androidx.room.ColumnInfo
+//import androidx.room.Entity
+//import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Lugar(
+    var id: Int,
+    val nombre: String,
+    val correo: String?,
+    val telefono: String?,
+    val web: String?,
+    val latitud: Double?,
+    val longitud: Double?,
+    val altura: Double?,
+    val rutaAudio: String?,
+    val rutaImagen: String?
+) : Parcelable{
+    constructor():
+            this("","","","","",0.0,0.0,0.0,"","")
+}
